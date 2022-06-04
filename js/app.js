76516bb0f4020de578a02e12310b2750f23e9a92
Noah Radovanovic <noah.r@rescout.de>
2022-06-04 14:05:10 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,53 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    //Toggle Nav
+    burger.addEventListener('click',() => {
+        nav.classList.toggle('nav-active');
+
+        //Animate links
+        navLinks.forEach((link, index) => {
+            if(link.style.animation) {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}`;
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}`;
+            }
+        });
+        //Burger animation
+        burger.classList.toggle('toggle');
+
+    });
+
+
+}
+
+$(document).ready(function () {
+    
+    $('#card-1').click(function () {
+        window.open("/examples/Pflanzenwiki/index.html");
+    })
+
+    $('#card-2').click(function () {
+        window.open("/examples/Museum_of_Candy/index.html");
+    });
+
+    $('#card-3').click(function () {
+        alert("Swift Datei kann nicht im Browser geöffnet werden.")
+    });
+});
+
+
+
+$(window).on("load", function () {
+    $(".loader-wrapper").delay(1350).fadeOut("slow");
+    ($("body").removeClass("preload"));
+});
+
+
+navSlide();
+
+
+
+
